7db1bc626924897c9f4e0c0de25802495988ef15
Jakestep <jakestep.17@gmail.com>
2023-02-27 20:03:44 -0700

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+TODO Get one dice working: 
+D20 - 
+TODO button,
+TODO input field for how many,
+TODO input field for DC,
+TODO total of the rolls,
+TODO how many passed the DC,
+TODO what percentage passed the DC.
+*/
