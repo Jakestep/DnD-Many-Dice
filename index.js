6338fd2a63061a77f459fc2020dcde81e96ad089
Jakestep <jakestep.17@gmail.com>
2023-02-28 12:03:35 -0700

    2/28

--- index.js
+++ index.js
@@ -16,30 +16,35 @@ const dcPassEl = document.getElementById('dc-pass-el');
 const dcPassElInitial = document.getElementById('dc-pass-el').textContent;
 const sidesEl = document.getElementById('sides')
 
-function howManyPassed(times, sides, dc) {
+function howManyPassed(times, sides, dc, modifier) {
     console.log(`DC: ${dc} Sides: ${sides} Number Of Rolls: ${times}`)
     console.log(sides)
     let didPass = 0;
     let didNotPass = 0;
     let crit = 0;
+    let critFail = 0;
     for (let i = 0; i < times; i++) {
         let tempNum = Math.floor(Math.random()*sides) + 1;
         console.log(tempNum)
-        if (tempNum >= dc) {
+        if (tempNum + parseInt(modifier) >= dc) {
             didPass++;
-            if (tempNum == 20) {
-                crit++
-            }
-        } else {
+            
+        } else if (tempNum == 1) {
+            critFail ++
+        } else if (tempNum == 20) {
+            crit++
+        }
+        else {
             didNotPass++;
         };
     };
     dcPassEl.textContent = dcPassElInitial + didPass
     document.getElementById('dc-fail-el').textContent = `This many failed: ${didNotPass}`
     document.getElementById('crit-el').textContent = `This many crit: ${crit}`
+    document.getElementById('crit-fail-el').textContent = `This many crit failed: ${critFail}`
 }
 
 rollAll.addEventListener('click', function() {
     // totalOfRolls(amountInput.textContent)
-    howManyPassed(amountInput.value, sidesEl.value, dcEl.value);
+    howManyPassed(amountInput.value, sidesEl.value, dcEl.value, document.getElementById('mod-el').value);
 });
